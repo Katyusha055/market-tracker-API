3c1a226ccf8588b919e8a7e61154b067d505211d
Katyusha <katyusha055@proton.me>
2026-01-05 22:04:26 -0500

    Changes added: \n -Finished setting up the logging system for the program \n -Added tests for all cases in the scrapper

--- tests/test_scrapper.py
+++ tests/test_scrapper.py
@@ -1,7 +1,9 @@
 import pytest
+import requests
 from program import web_scrapper as ws
 
-def test_scrapper(monkeypatch):
+#testing the "happy path" or normal case
+def test_scrapper_1(monkeypatch):
     FAKE_HTML = """
     <html>
       <body>
@@ -27,3 +29,57 @@ def test_scrapper(monkeypatch):
     result = ws.newegg_scrapper()
 
     assert result == [{'description':'RAM stick', 'price':'123'}]
+
+#testing the case where the connection goes wrong
+def test_scrapper_2(monkeypatch):
+    def fake_get(url, timeout):
+        raise requests.exceptions.ConnectionError('No connection')
+
+    monkeypatch.setattr(ws.requests, "get", fake_get)
+
+    with pytest.raises(ConnectionError):
+        ws.newegg_scrapper()
+
+#testing invalid result of request
+def test_scrapper_3(monkeypatch):
+    class FakeResponse:
+        def __init__(self, status_code=200):
+            self.status_code = status_code
+
+    def fake_get(url, timeout):
+        time = timeout
+        return FakeResponse(404)
+
+    monkeypatch.setattr(ws.requests, "get", fake_get)
+
+    with pytest.raises(ConnectionError):
+        ws.newegg_scrapper()
+
+#testing case where the data is incomplete
+def test_scrapper_4(monkeypatch, caplog):
+    FAKE_HTML = """
+    <html>
+      <body>
+        <div class="item-cell">
+          <div class="item-title">Broken RAM</div>
+        </div>
+      </body>
+    </html>
+    """
+
+    class FakeResponse:
+        def __init__(self, text, status_code=200):
+            self.text = text
+            self.status_code = status_code
+
+    def fake_get(url, timeout):
+        timeout = timeout
+        return FakeResponse(FAKE_HTML)
+
+    monkeypatch.setattr(ws.requests, "get", fake_get)
+
+    with caplog.at_level("WARNING"):
+        result = ws.newegg_scrapper()
+
+    assert result == []  # item has been skipped
+    assert "does not have price and/or description" in caplog.text
