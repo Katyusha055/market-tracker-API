5407d36e8a6ed0f88086f367782abbb5566f2435
Katyusha_055 <katyusha055@proton.me>
2026-02-24 18:28:04 -0500

    Fix sql normalizer test to expect gb_per_module for single module

--- tests/test_sql_utils.py
+++ tests/test_sql_utils.py
@@ -4,7 +4,7 @@ from program.normalizers import sql_utils as squ
 def test1():
     data = {'Ram Quantity': ['16'], 'Ram Type': ['DDR4'], 'Ram Speed': ['3200'], 'Ram Brand': ['G.Skill'], 'Original String': 'G.SKILL Ripjaws V Series 16GB 288-Pin PC RAM DDR4 3200 (PC4 25600) Desktop Memory Model F4-3200C16S-16GVK', 'Price': '$129.99–', 'Source': 'Newegg'}
     result = squ.sql_normalizer(data)
-    assert result == squ.RamProduct(brand='G.Skill', ram_type='DDR4', speed=3200, total_gb=16, modules=1, gb_per_module=None, price=129.99, source='Newegg', raw_listing='G.SKILL Ripjaws V Series 16GB 288-Pin PC RAM DDR4 3200 (PC4 25600) Desktop Memory Model F4-3200C16S-16GVK')
+    assert result == squ.RamProduct(brand='G.Skill', ram_type='DDR4', speed=3200, total_gb=16, modules=1, gb_per_module=16, price=129.99, source='Newegg', raw_listing='G.SKILL Ripjaws V Series 16GB 288-Pin PC RAM DDR4 3200 (PC4 25600) Desktop Memory Model F4-3200C16S-16GVK')
 
 # 2 modules case
 def test2():
